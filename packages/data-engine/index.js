4d5627abf3ce5423f6948d5fa0e456ccf26e4ba3
Šilhan Jan <jan.silhan@ixperta.com>
2017-05-04 13:34:59 +0200

    Fixed data-engine exports

--- packages/data-engine/index.js
+++ packages/data-engine/index.js
@@ -3,12 +3,7 @@
 
 var def;
 if (process.env.NODE_ENV === 'production') {
-  def = require('./cjs/data-engine.production.min.js')
+  module.exports = require('./cjs/data-engine.production.min.js')
 } else {
-  def = require('./cjs/data-engine.development.js');
+  module.exports = require('./cjs/data-engine.development.js');
 }  
-
-module.exports['default'] = def.default;
-module.exports.FilterValue = def.FilterValue;
-module.exports.Filter = def.Filter;
-module.exports.Sort = def.Sort;
