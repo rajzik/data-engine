4860a13f5cafc2cc1d958c28e42d04515eb306ac
silhan Jan <jan.silhan@ixperta.com>
2018-03-13 14:19:08 +0100

    test

--- scripts/circleci/set_up_github_keys.sh
+++ scripts/circleci/set_up_github_keys.sh
@@ -4,9 +4,9 @@ set -e
 
 if [ -n $GITHUB_TOKEN ]; then
 
-  GH_PAGES_DIR=`pwd`/../react-gh-pages
+  GH_PAGES_DIR=`pwd`/../circle-ci-bot-data-engine
   echo "machine github.com login data-engine-bot password $GITHUB_TOKEN" >~/.netrc
   git config --global user.name "Circle CI"
-  git config --global user.email "circle@data-engine.org"
+  git config --global user.email "circle.bot.jansilhan@gmail.com"
 
 fi
