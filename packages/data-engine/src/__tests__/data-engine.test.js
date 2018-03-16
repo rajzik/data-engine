bb69ebef292e861ec9721f3ce12ec85d3191fd44
silhan Jan <jan.silhan@ixperta.com>
2018-03-16 17:04:47 +0100

    Fixed __test__ var

--- packages/data-engine/src/__tests__/data-engine.test.js
+++ packages/data-engine/src/__tests__/data-engine.test.js
@@ -44,4 +44,24 @@ describe('Data engine', () => {
         ];
         expect(dataEngine.Data.length).not.toBe(0);
     });
+    it('should not be empty array', () => {
+        const dataEngine = new DataEngine();
+        dataEngine.Data = [
+            { a: 10, },
+            { a: 1521129090606, },
+            { a: 1521129090606, },
+            { a: 13, },
+            { a: 14, },
+            { a: 15, },
+            { a: 16, },
+            { a: 17, },
+            { a: 18, },
+            { a: 19, },
+            { a: 12, },
+            { a: 11, }
+        ];
+        dataEngine.addFilter('a', { from: 1521129090606, to: null, });
+        console.log(dataEngine.Data);
+        expect(dataEngine.Data.length).not.toBe(0);
+    });
 });
--- scripts/jest/setupEnvironment.js
+++ scripts/jest/setupEnvironment.js
@@ -1,12 +1,11 @@
 /* eslint-disable */
 
 const NODE_ENV = process.env.NODE_ENV;
-const IS_TEST = process.env.IS_TEST;
-if (NODE_ENV !== 'development' && NODE_ENV !== 'production') {
+if (NODE_ENV !== 'development' && NODE_ENV !== 'production' && NODE_ENV !== 'test') {
     throw new Error('NODE_ENV must either be set to development or production.');
 }
 global.__DEV__ = NODE_ENV === 'development';
-global.__TEST__ = IS_TEST === 'test';
+global.__TEST__ = NODE_ENV === 'test';
 
 
 global.requestAnimationFrame = (callback) => {
